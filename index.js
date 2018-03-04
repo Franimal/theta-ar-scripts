f3d6995b426c879865a6d3266e21139928f35b24
TimKingTheta <timothy.king@theta.co.nz>
2018-03-05 10:29:47 +1300

    Change json a little

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 {
 "IOS":[],
 "MR":[
-{"end-to-end-example.js":{
+{
+	"filename":"end-to-end-example.js",
 	"title":"End to End Example", 
 	"description":"A script containing all of the key features of the scripting api.", 
 	"author":"Theta",
 	"version":"1.0",
-	"thumbnail":"https://www.theta.co.nz/images/thetaLogo_sm.png"}}
+	"thumbnail":"https://www.theta.co.nz/images/thetaLogo_sm.png"
+}
 ]
 }
