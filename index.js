a4c93f3f76fc0c5e7dd3558cc1ed43bc13d86767
TimKingTheta <timothy.king@theta.co.nz>
2018-03-05 10:23:14 +1300

    Add end-to-end-example to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+{
+"IOS":[],
+"MR":[
+{"end-to-end-example.js":{
+	"title":"End to End Example", 
+	"description":"A script containing all of the key features of the scripting api.", 
+	"author":"Theta",
+	"version":"1.0",
+	"thumbnail":"https://www.theta.co.nz/images/thetaLogo_sm.png"}}
+]
+}
