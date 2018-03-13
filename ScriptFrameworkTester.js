50b943cd74701859a1f62de5896a530c0d731b04
TimKingTheta <timothy.king@theta.co.nz>
2018-03-13 14:19:33 +1300

    api changes

--- ScriptFrameworkTester.js
+++ ScriptFrameworkTester.js
@@ -7,15 +7,15 @@ function start(){
 	var ob = createCube("Left", new Vector3(-0.5, 0, 2), new Vector3(0, 0, 0), new Vector3(0.4, 0.4, 0.4));
 	
 	var moveBehaviours = [
-		transitionPos("a", 1, 1, 1, 1, setMoveKey("b")), 
-		transitionPos("b", 1, 2, 1, 1, setMoveKey("c")), 
-		transitionPos("c", 1, 1, 2, 1, setMoveKey("a"))
+		transitionPos("a", 1, 1, 1, 1, setState(ob, "colourKey", "b")), 
+		transitionPos("b", 1, 2, 1, 1, setState(ob, "colourKey", "c")), 
+		transitionPos("c", 1, 1, 2, 1, setState(ob, "colourKey", "a"))
 	];
 
 	var colourBehaviours = [
-		transitionColour("a", 255, 0, 0, 1, setState("colourKey", "b")), 
-		transitionColour("b", 255, 0, 255, 1, setState("colourKey", "c")), 
-		transitionColour("c", 0, 255, 255, 1, setState("colourKey", "a"))
+		transitionColour("a", 255, 0, 0, 1, setState(ob, "colourKey", "b")), 
+		transitionColour("b", 255, 0, 255, 1, setState(ob, "colourKey", "c")), 
+		transitionColour("c", 0, 255, 255, 1, setState(ob, "colourKey", "a"))
 	];
 	
 	setState(ob, 'moveKey', 'a');
