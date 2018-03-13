2f1f9346d9d2a86f12577a7fdca255a4e0bca5c1
TimKingTheta <timothy.king@theta.co.nz>
2018-03-14 12:43:25 +1300

    Add logs to transitionPos

--- ScriptFrameworkTester.js
+++ ScriptFrameworkTester.js
@@ -12,27 +12,25 @@ function start(){
 		//})
 		//.addBehaviours(moveBehaviours)
 		//.addBehaviours(colourBehaviours);
-		log("1");
+
 		var moveInstructions = [
 			transitionPos("a", 1, 1, 1, 1, setState(ob, "colourKey", "b")), 
 			transitionPos("b", 1, 2, 1, 1, setState(ob, "colourKey", "c")), 
 			transitionPos("c", 1, 1, 2, 1, setState(ob, "colourKey", "a"))
 		];
-		log("2");
+
 		var colourInstructions = [
 			transitionColour("a", 255, 0, 0, 1, setState(ob, "colourKey", "b")), 
 			transitionColour("b", 255, 0, 255, 1, setState(ob, "colourKey", "c")), 
 			transitionColour("c", 0, 255, 255, 1, setState(ob, "colourKey", "a"))
 		];
-		log("3");
 	setState(ob, "moveKey", "a");
-		log("4");
+
 	setState(ob, "colourKey", "a");
-		log("5");
+
 	addInstructions(ob, colourInstructions);
-	log("6");
+
 	addInstructions(ob, moveInstructions);
-	log("7");
 }
 
 function update(){
