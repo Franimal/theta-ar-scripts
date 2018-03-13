bb14289ba8eacf835acbe8b3ee5ecdf8cbeee7e3
TimKingTheta <timothy.king@theta.co.nz>
2018-03-13 15:12:30 +1300

    A

--- ScriptFrameworkTester.js
+++ ScriptFrameworkTester.js
@@ -12,24 +12,27 @@ function start(){
 		//})
 		//.addBehaviours(moveBehaviours)
 		//.addBehaviours(colourBehaviours);
-	
+		log("1");
 		var moveBehaviours = [
 			transitionPos("a", 1, 1, 1, 1, setState(ob, "colourKey", "b")), 
 			transitionPos("b", 1, 2, 1, 1, setState(ob, "colourKey", "c")), 
 			transitionPos("c", 1, 1, 2, 1, setState(ob, "colourKey", "a"))
 		];
-
-	var colourBehaviours = [
+		log("2");
+		var colourBehaviours = [
 			transitionColour("a", 255, 0, 0, 1, setState(ob, "colourKey", "b")), 
 			transitionColour("b", 255, 0, 255, 1, setState(ob, "colourKey", "c")), 
 			transitionColour("c", 0, 255, 255, 1, setState(ob, "colourKey", "a"))
 		];
-	
+		log("3");
 	setState(ob, 'moveKey', 'a')();
+		log("4");
 	setState(ob, 'colourKey', 'a')();
-	
+		log("5");
 	applyBehaviours(ob, colourBehaviours);
+	log("6");
 	applyBehaviours(ob, moveBehaviours);
+	log("7");
 }
 
 function update(){
