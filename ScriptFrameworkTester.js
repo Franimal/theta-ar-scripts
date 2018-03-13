17bc3edbc9faf6d22f11ae62b5003eb9dba3ce64
TimKingTheta <timothy.king@theta.co.nz>
2018-03-14 12:40:35 +1300

    move applyBehaviours to JS

--- ScriptFrameworkTester.js
+++ ScriptFrameworkTester.js
@@ -13,13 +13,13 @@ function start(){
 		//.addBehaviours(moveBehaviours)
 		//.addBehaviours(colourBehaviours);
 		log("1");
-		var moveBehaviours = [
+		var moveInstructions = [
 			transitionPos("a", 1, 1, 1, 1, setState(ob, "colourKey", "b")), 
 			transitionPos("b", 1, 2, 1, 1, setState(ob, "colourKey", "c")), 
 			transitionPos("c", 1, 1, 2, 1, setState(ob, "colourKey", "a"))
 		];
 		log("2");
-		var colourBehaviours = [
+		var colourInstructions = [
 			transitionColour("a", 255, 0, 0, 1, setState(ob, "colourKey", "b")), 
 			transitionColour("b", 255, 0, 255, 1, setState(ob, "colourKey", "c")), 
 			transitionColour("c", 0, 255, 255, 1, setState(ob, "colourKey", "a"))
@@ -29,9 +29,9 @@ function start(){
 		log("4");
 	setState(ob, "colourKey", "a");
 		log("5");
-	applyBehaviours(ob, colourBehaviours);
+	addInstructions(ob, colourInstructions);
 	log("6");
-	applyBehaviours(ob, moveBehaviours);
+	addInstructions(ob, moveInstructions);
 	log("7");
 }
 
