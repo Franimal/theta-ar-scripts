9e2dc2ecbc542aeddce68fd153779a9baf28536f
TimKingTheta <timothy.king@theta.co.nz>
2018-03-15 09:58:08 +1300

    fix color variable/function names.  Fix color lerping (wasn't meant to be in 255 space)

--- ScriptFrameworkTester.js
+++ ScriptFrameworkTester.js
@@ -2,41 +2,48 @@ function setup(){
 
 }
 
-function start(){	
-	var ob = createCube("Left", new Vector3(-0.5, 0, 2), new Vector3(0, 0, 0), new Vector3(0.4, 0.4, 0.4))
-		//.setState(function (currentState) {
-		//	return {
-		//		moveKey: 'a',
-		//		colourKey: 'a'
-		//	};
-		//})
-		//.addBehaviours(moveBehaviours)
-		//.addBehaviours(colourBehaviours);
-
-		var moveInstructions = [
-			transitionPos("a", 1, 1, 1, 1, setState(ob, "colourKey", "b")), 
-			transitionPos("b", 1, 2, 1, 1, setState(ob, "colourKey", "c")), 
-			transitionPos("c", 1, 1, 2, 1, setState(ob, "colourKey", "a"))
-		];
-
-		var colourInstructions = [
-			transitionColour("a", 255, 0, 0, 1, setState(ob, "colourKey", "b")), 
-			transitionColour("b", 255, 0, 255, 1, setState(ob, "colourKey", "c")), 
-			transitionColour("c", 0, 255, 255, 1, setState(ob, "colourKey", "a"))
-		];
+function setKey(keyName, keyValue) {
+	return function(instructionComponent) {
+		if (!instructionComponent.state) {
+			instructionComponent.state = {};
+		}
 		
-		var testInstructions = [
-			instructionTest()
-		];
-	setState(ob, "moveKey", "a");
+		instructionComponent.state[keyName] = keyValue;
+	}
+}
 
-	setState(ob, "colourKey", "a");
-
-	//addInstructions(ob, colourInstructions);
-
-	//addInstructions(ob, moveInstructions);
+function start(){	
+	var obj = createCube("Left", new Vector3(-0.5, 0, 2), new Vector3(0, 0, 0), new Vector3(0.4, 0.4, 0.4))
 	
-	addInstructions(ob, testInstructions);
+	setState(obj, function (currentState) {
+		return {
+			moveKey: 'a',
+			colorKey: 'a',
+			testKey: 'a'
+		};
+	});
+
+	var moveInstructions = [
+		transitionPos("a", 1, 1, 1, 1, setKey("moveKey", "b")), 
+		transitionPos("b", 1, 2, 1, 1, setKey("moveKey", "c")), 
+		transitionPos("c", 1, 1, 2, 1, setKey("moveKey", "a"))
+	];
+
+	var colorInstructions = [
+		transitionColor("a", 1, 0, 0, 1, 1, setKey("colorKey", "b")), 
+		transitionColor("b", 0, 1, 0, 1, 1, setKey("colorKey", "c")), 
+		transitionColor("c", 0, 0, 1, 1, 1, setKey("colorKey", "a"))
+	];
+	
+	var testInstructions = [
+		instructionTest("a", 1, 1, 1, 1, setKey("testKey", "b"))
+	];
+
+	 addInstructions(obj, colorInstructions);
+
+	 addInstructions(obj, moveInstructions);
+	
+	 addInstructions(obj, testInstructions);
 	
 
 }
