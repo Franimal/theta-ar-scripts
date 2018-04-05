4ed729a10b070f81afdcf9eb3cbd37255075e17f
TimKingTheta <timothy.king@theta.co.nz>
2018-04-05 12:12:59 +1200

    A

--- index.js
+++ index.js
@@ -47,11 +47,20 @@
 	"thumbnail":"https://www.theta.co.nz/images/thetaLogo_sm.png"
 },
 {
-	"filename":"C:/Projects/theta-ar-scripts/InnovationLab.js",
+	"filename":"C:/Projects/theta-ar-scripts/PGWScenario.js",
 	"includes":["C:/Projects/theta-ar-scripts/lib/game-object-wrapper.js"],
-	"title":"LOCAL: Innovation Lab Showcase", 
+	"title":"LOCAL: PGW", 
 	"description":"A showcase of the recent work of the Innovation Lab.", 
 	"author":"Theta",
+	"version":"1.0",
+	"thumbnail":"https://www.theta.co.nz/images/thetaLogo_sm.png"
+},
+{
+	"filename":"https://raw.githubusercontent.com/Franimal/theta-ar-scripts/master/PGWScenario.js",
+	"includes":["https://raw.githubusercontent.com/Franimal/theta-ar-scripts/master/lib/game-object-wrapper.js"],
+	"title":"PGW Scenario", 
+	"description":"Scenario for PGW.", 
+	"author":"You",
 	"version":"1.0",
 	"thumbnail":"https://www.theta.co.nz/images/thetaLogo_sm.png"
 },
