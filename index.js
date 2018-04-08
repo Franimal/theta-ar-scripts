3f2d81c6524cc91ef491f3c12516a7404058fae5
TimKingTheta <timothy.king@theta.co.nz>
2018-04-09 10:42:43 +1200

    Remove pgw scripts from index.js

--- index.js
+++ index.js
@@ -45,24 +45,6 @@
 	"author":"Theta",
 	"version":"1.0",
 	"thumbnail":"https://www.theta.co.nz/images/thetaLogo_sm.png"
-},
-{
-	"filename":"C:/Projects/theta-ar-scripts/PGWScenario.js",
-	"includes":["C:/Projects/theta-ar-scripts/lib/game-object-wrapper.js"],
-	"title":"LOCAL: PGW", 
-	"description":"A showcase of the recent work of the Innovation Lab.", 
-	"author":"Theta",
-	"version":"1.0",
-	"thumbnail":"https://www.theta.co.nz/images/thetaLogo_sm.png"
-},
-{
-	"filename":"https://raw.githubusercontent.com/Franimal/theta-ar-scripts/master/PGWScenario.js",
-	"includes":["https://raw.githubusercontent.com/Franimal/theta-ar-scripts/master/lib/game-object-wrapper.js"],
-	"title":"PGW Scenario", 
-	"description":"Scenario for PGW.", 
-	"author":"You",
-	"version":"1.0",
-	"thumbnail":"https://www.theta.co.nz/images/thetaLogo_sm.png"
 }
 ]
 }
